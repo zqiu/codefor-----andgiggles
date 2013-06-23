6610344354f723f737f55e2897559344ef7c1b83
zqiu <zqiu1994@gmail.com>
2013-06-23 00:56:04 -0400

    added main.c and todo.txt main currently only has the file io done

---
+++ passwordencryption/main.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+	bool encrypt;
+	FILE * readf, writef;
+	char * filename,tmp,data;
+	if(argc != 3){
+		printf("error: need 2 arguments. Name of file and 0 for encryption\n");
+		exit(EXIT_FAILURE);
+	}
+	encrypt = (atoi(argv[2])==0?1:0);
+	filename = strtok(argv[1],".");
+	strcat(tmp,filename);
+	if(encrypt){
+		if(!strcmp(strtok(NULL),"txt"){
+			printf("error: first argument is not a text file\n");
+			exit(EXIT_FAILURE);
+		}
+		readf = fopen(argv[1],"r");
+		if(!readf){
+			printf("error: file does not exist\n");
+			exit(EXIT_FAILURE);
+		}
+		writef = fopen(strcat(tmp,".q"),"r");
+		if(writef){
+			printf("file %s already exist. continue y?",tmp);
+			scanf("%c",data);
+			if(!strcmp(data,"y")){
+				exit(EXIT_SUCCESS);
+			}
+		}
+		fclose(writef);
+		writef = fopen(tmp,"w");
+		
+		//code to come
+		
+		fclose(readf);
+		fclose(writef);
+		printf("thank you for using this encryption software\n");
+		exit(EXIT_SUCCESS);
+	}
+}
