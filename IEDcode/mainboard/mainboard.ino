3042f0dacab9b1f296e610efd6204bf43f670cdd
zqiu <zqiu1994@gmail.com>
2013-11-01 00:35:33 -0400

    begining to work on the main module. it's mainly a copy of the recieve from the example

--- IEDcode/mainboard/mainboard.ino
+++ IEDcode/mainboard/mainboard.ino
@@ -1,7 +1,33 @@
+
+/*code for the main board*/
+#include <RFM12B.h>
+
+#define NODEID           1  //network ID used for this unit
+#define NETWORKID       99  //the network ID we are on
+#define SERIAL_BAUD 115200
+
+// Need an instance of the Radio Module
+RFM12B radio;
+
 void setup(){
-  
+  radio.Initialize(NODEID, RF12_433MHZ, NETWORKID);
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Listening...");
 }
 
 void loop(){
-  
+  if (radio.ReceiveComplete()){
+    if (radio.CRCPass()){
+      Serial.print('[');Serial.print(radio.GetSender());Serial.print("] ");
+      for (byte i = 0; i < *radio.DataLen; i++) //can also use radio.GetDataLen() if you don't like pointers
+        Serial.print((char)radio.Data[i]);
+      if (radio.ACKRequested()){
+        radio.SendACK();
+        Serial.print(" - ACK sent");
+      }
+    }
+    else
+      Serial.print("BAD-CRC");
+    Serial.println();
+  }
 }
