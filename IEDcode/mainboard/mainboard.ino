2a6de54f77a5c9db066add1457119788c2a16b4e
zqiu <zqiu1994@gmail.com>
2013-11-03 11:53:54 -0500

    program askes person to input initial information now and does bound checking on vitals

--- IEDcode/mainboard/mainboard.ino
+++ IEDcode/mainboard/mainboard.ino
@@ -5,16 +5,38 @@
 #define NODEID           1  //network ID used for this unit
 #define NETWORKID       99  //the network ID we are on
 #define SERIAL_BAUD 115200
+#define DELAYONREAD     10  //milliseconds to wait before reading
 
 // Need an instance of the Radio Module
 RFM12B radio;
-byte temp,res,heart;
+int temp,res,heart;
 char scratch[10];
+bool male = false;
+byte age,weight,restingrate,temporary;
 
 void setup(){
-  //TODO:init char. of the person. (sex,age,etc...)
-  radio.Initialize(NODEID, RF12_433MHZ, NETWORKID);
   Serial.begin(SERIAL_BAUD);
+  while(!male){
+    Serial.println("are you a male?y/n");
+    waitforinput();
+    temporary = Serial.read();
+    if (temporary == 'y'){
+      male = true;
+    }else if(temporary == 'n'){
+      break;
+    }
+  }
+  Serial.println("enter age:");
+  waitforinput();
+  age = constrain(Serial.parseInt(), 0, 255);
+  Serial.println("enter weight in kilograms:");
+  waitforinput();
+  weight = constrain(Serial.parseInt(), 0, 255);
+  Serial.println("enter resting heart rate:");
+  waitforinput();
+  restingrate = constrain(Serial.parseInt(), 0, 255);
+  temporary = (byte)(206.9 - (0.67 * age));
+  radio.Initialize(NODEID, RF12_433MHZ, NETWORKID);
   Serial.println("Listening...");
 }
 
@@ -22,14 +44,23 @@ void loop(){
   if (radio.ReceiveComplete()){
     if (radio.CRCPass()){
       Serial.print('[');Serial.print(radio.GetSender());Serial.print("] ");
-      copy((char*)radio.Data,scratch,0,10);
+      copy(scratch,(char*)radio.Data,0,10);
       temp = atoi(scratch);
-      copy((char*)radio.Data,scratch,10,10);
+      copy(scratch,(char*)radio.Data,10,10);
       res = atoi(scratch);
-      copy((char*)radio.Data,scratch,20,10);
+      copy(scratch,(char*)radio.Data,20,10);
       heart = atoi(scratch);
       printinfo(temp,res,heart);
       //TODO:convert readings of voltage value to measured values
+      if(res < 10 || res > 55){
+        Serial.println("respitory rate out of range");
+      }
+      if(heart > temporary || heart < restingrate){
+        Serial.println("heartrate out of range");
+      }
+      if(temp > 40 || temp < 35){
+        Serial.println("temp out of range");      
+      }
       if (radio.ACKRequested()){
         radio.SendACK();
         Serial.print(" - ACK sent");
@@ -53,5 +84,11 @@ void printinfo(int temperature, int resprate, int heartrate){
 void copy(char * to, char * from, byte beg, byte num){
   for(byte i = 0; i < num; ++i){
     to[i+beg] = from[i];
+  }
+}
+
+void waitforinput(){
+  while(Serial.available() == 0){
+    delay(DELAYONREAD);
   }
 }
