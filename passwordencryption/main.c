6b425da2090fcc26dfe85024530583f1f4a4d5e1
zqiu <zqiu1994@gmail.com>
2014-05-23 00:14:19 -0400

    fixed way the first arg of program is interpreted to make files

--- passwordencryption/main.c
+++ passwordencryption/main.c
@@ -7,16 +7,16 @@
 int main(int argc, char **argv){
 	bool encrypt;
 	FILE * readf, * writef;
-	char *filename, *tmp, *password, *data;
+	char *filename, tmp[20], password[20], data[20];
 	if(argc != 3){
 		printf("error: need 2 arguments. Name of file and 0 for encryption\n");
 		exit(EXIT_FAILURE);
 	}
 	encrypt = (atoi(argv[2])==0?1:0);
-	filename = strtok(argv[1],".");
-	strcpy(tmp,filename);
+	strcpy(tmp,argv[1]);
+	filename = strtok(tmp,".");
 	if(encrypt){
-		if(!strcmp(strtok(NULL,"."),"txt\n")){
+		if(strcmp(strtok(NULL,"."),"txt")){
 			printf("error: first argument is not a text file\n");
 			exit(EXIT_FAILURE);
 		}
@@ -25,18 +25,18 @@ int main(int argc, char **argv){
 			printf("error: file does not exist\n");
 			exit(EXIT_FAILURE);
 		}
-		writef = fopen(strcat(tmp,".q\n"),"r");
+		writef = fopen(strcat(filename,".q"),"r");
 		if(writef){
-			printf("file %s already exist. continue y? \n",tmp);
-			scanf("%c",data);
-			if(!strcmp(data,"y")){
+			printf("file %s already exist. continue y? \n",filename);
+			scanf("%s",data);
+			if(strcmp(data,"y")){
 				exit(EXIT_SUCCESS);
 			}
 		}
 		fclose(writef);
-		writef = fopen(tmp,"w");
+		writef = fopen(filename,"w");
 		printf("password?\n");
-		scanf("%c",password);
+		scanf("%s",password);
 		if(strlen(password) > 20 || strlen(password) < 1 ){
 			printf("error: password must be less than 20 char and not be empty\n");
 			exit(EXIT_FAILURE);
@@ -47,8 +47,8 @@ int main(int argc, char **argv){
 		fclose(readf);
 		fclose(writef);
 		printf("thank you for using this encryption software\n");
-		exit(EXIT_SUCCESS);
 	}
+	exit(EXIT_SUCCESS);
 }
 
 bool readFile(FILE * readf, char * data){
