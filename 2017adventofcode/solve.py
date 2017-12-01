6da67700867ded9aa5106e0a1eab54163424697b
zqiu <zqiu1994@gmail.com>
2017-12-01 00:52:55 -0500

    cleaned up solution

--- 2017adventofcode/solve.py
+++ 2017adventofcode/solve.py
@@ -1,18 +1,13 @@
 f = open("input")
 ans = 0
 ans2 = 0
-first = ""
 for line in f:
 	size = len(line)
-	for i in range(size - 1):
-		if i == 0:
-			first = line[0]
-		if line[i] == line[i+1]:
+	for i in range(size):
+		if line[i] == line[(i+1)%size]:
 			ans += int(line[i])
 	for i in range(size):
 		if line[i] == line[ (i + size/2) % size]:
 			ans2 += int(line[i])
-	if first == line[size - 1]:
-		ans += int(first)
 print ans
 print ans2
